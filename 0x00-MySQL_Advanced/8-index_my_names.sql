21fb0ac066d4437101b34e952cfd8ae3d45fe037
Ukeme Edet <ukemeedet2207@gmail.com>
2024-10-19 20:55:30 +0100

    Add SQL script to create an index on the first letter of the 'name' column in the 'names' table

---
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -0,0 +1,4 @@
+-- A SQL script that creates an index `idx_name_first` on the table `names` and the the first letter of `name`
+-- Requirements:
+-- - Only the first letter of `name` must be indexed
+CREATE INDEX idx_name_first ON names(name(1));
