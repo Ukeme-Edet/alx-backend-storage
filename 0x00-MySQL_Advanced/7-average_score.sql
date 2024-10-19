8bf76440bdea96ff871e9b2a359585582f72c8d9
Ukeme Edet <ukemeedet2207@gmail.com>
2024-10-19 20:09:37 +0100

    Refactor SQL script to compute and store average score for a user

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,23 +1,18 @@
--- A SQL script that creates a stored procedure `AverageScoreForUser` that computes the average score for a user
+-- A SQL script that creates a stored procedure `ComputeAverageScoreForUser` that computes and stores the average score for a student
 -- Requirements:
--- - Procedure `AverageScoreForUser` takes 1 input:
+-- - Procedure `ComputeAverageScoreForUser` takes 1 input:
 -- - - `user_id`, a users.id (`user_id` is linked to an existing `users`)
 DELIMITER //
-CREATE PROCEDURE AverageScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-	DECLARE avg_score DECIMAL(5, 2);
+	DECLARE avg_score FLOAT;
 	
 	-- Compute the average score
 	SELECT AVG(score) INTO avg_score
 	FROM corrections
-	WHERE user_id = user_id;
+	WHERE corrections.user_id = user_id;
 	
-	-- If the user has no score, set the average to 0
-	IF avg_score IS NULL THEN
-		SET avg_score = 0;
-	END IF;
-
-	-- Set the average score for the user
+	-- Update the user's average score
 	UPDATE users
 	SET average_score = avg_score
 	WHERE id = user_id;
